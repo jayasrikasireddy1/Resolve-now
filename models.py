5f8cbfca0cc19e290ffe8b7dcb3fe5b4c847d294
jayasrikasireddy1 <jayasrikasireddy1@gmail.com>
2025-07-02 12:45:58 +0530

    Add files via upload

---
+++ models.py
@@ -0,0 +1,13 @@
+from database import db
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), unique=True, nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+
+class Complaint(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    subject = db.Column(db.String(200))
+    description = db.Column(db.Text)
+    status = db.Column(db.String(20), default='Pending')
